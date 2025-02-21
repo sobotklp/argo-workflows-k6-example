23055a22486cc43ac83415214fba2fdaf802cec8
Lewis Sobotkiewicz <lewis.sobotkiewicz@wealthsimple.com>
2025-02-21 13:26:19 -0700

    Initial commit

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+KUBE_CONTEXT=docker-desktop
+NAMESPACE=argo-workflows
+
+# Create namespace with auto-injection
+kubectl create namespace $NAMESPACE --context=$KUBE_CONTEXT
+kubectl label namespace $NAMESPACE istio-injection=enabled --context=$KUBE_CONTEXT
+
+# Install services
+helm upgrade --install --kube-context=$KUBE_CONTEXT -n=$NAMESPACE --create-namespace argo-workflows .
+
+# Forward the Server's port to access the UI:
+# kubectl -n $NAMESPACE --context=$KUBE_CONTEXT port-forward service/argo-workflows-server 2746:2746
